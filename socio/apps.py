6019af952fddae0653b946373e9c5a766826cfd6
Shreyas Shankar <shreyas.shankar920@gmail.com>
2018-05-01 07:49:57 +0530

    socio

--- socio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SocioConfig(AppConfig):
-    name = 'socio'
--- socio/wsgi.py
+++ socio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OOAD.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socio.settings")
 
 application = get_wsgi_application()
