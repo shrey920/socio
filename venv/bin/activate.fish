db283c3c1274242b971126f538ea23319d5b820e
Shreyas Shankar <shreyas.shankar920@gmail.com>
2019-03-04 14:26:42 +0530

    fine tuned

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -56,7 +56,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/shreyas/socio/virtual_env"
+set -gx VIRTUAL_ENV "/home/shreyas/socio/venv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
